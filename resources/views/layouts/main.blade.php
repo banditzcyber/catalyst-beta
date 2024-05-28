ac4737d1833232517d950089a824c3ce08b49140
Sobandi - HCM <sobandi.afandi@capcx.com>
2024-05-28 10:55:32 +0700

    update: 2805202

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,10 +4,18 @@
 <head>
 
     <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
+    <!-- Meta -->
+    <meta name="description" content="Responsive Bootstrap 4 Dashboard Template">
+    <meta name="author" content="ThemePixels">
+    <meta http-equiv="Content-Security-Policy" content="script-src 'self' https://fonts.googleapis.com">
 
     <!-- Favicon -->
     <link rel="shortcut icon" type="image/x-icon" href="#">
+
+
 
     <title>{{ $title }} | MyCatalyst</title>
 
