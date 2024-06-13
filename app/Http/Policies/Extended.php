39b93302d24708e0e62c5ecdbd8474f9216e8a30
Sobandi - HCM <sobandi.afandi@capcx.com>
2024-06-13 14:38:54 +0700

    update-local

--- app/Http/Policies/Extended.php
+++ app/Http/Policies/Extended.php
@@ -10,15 +10,15 @@ class Extended extends Basic
     {
         parent::configure();
 
-        $this
-            ->addDirective(Directive::STYLE, 'fonts.googleapis.com')
-            ->addDirective(Directive::DEFAULT, 'google.analytics.com')
-            ->addDirective(Directive::FONT, 'self')
-            ->addDirective(Directive::FONT, 'fonts.googleapis.com')
-            ->addDirective(Directive::FONT, 'fonts.gstatic.com');
-            // ->addDirective(Directive::SCRIPT_ELEM, 'self')
-            // ->addDirective(Directive::STYLE_ELEM, 'self');
-            // ->addNonceForDirective(Directive::SCRIPT_ELEM)
-            // ->addNonceForDirective(Directive::STYLE_ELEM);
+        // $this
+        //     ->addDirective(Directive::STYLE, 'fonts.googleapis.com')
+        //     ->addDirective(Directive::DEFAULT, 'google.analytics.com')
+        //     ->addDirective(Directive::FONT, 'self')
+        //     ->addDirective(Directive::FONT, 'fonts.googleapis.com')
+        //     ->addDirective(Directive::FONT, 'fonts.gstatic.com');
+        //     ->addDirective(Directive::SCRIPT_ELEM, 'self')
+        //     ->addDirective(Directive::STYLE_ELEM, 'self');
+        //     ->addNonceForDirective(Directive::SCRIPT_ELEM)
+        //     ->addNonceForDirective(Directive::STYLE_ELEM);
     }
 }
