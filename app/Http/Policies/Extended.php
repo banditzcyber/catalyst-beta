6b0a6626e0cb79b63a150dc6644e8f572ac38bdb
Sobandi - HCM <sobandi.afandi@capcx.com>
2024-06-19 08:56:50 +0700

    update

--- app/Http/Policies/Extended.php
+++ app/Http/Policies/Extended.php
@@ -11,7 +11,7 @@ class Extended extends Basic
         parent::configure();
 
             $this->addDirective(Directive::STYLE, ['self','fonts.googleapis.com']);
-            $this->addDirective(Directive::STYLE_ELEM, ['self', 'fonts.googleapis.com', 'unsafe-inline']);
+            $this->addDirective(Directive::STYLE_ELEM, ['self', 'fonts.googleapis.com', 'fonts.googleapis.com/css?family=Lato:300,400,700,900', 'unsafe-inline']);
             $this->addDirective(Directive::STYLE_ATTR, ['self', 'unsafe-inline', 'fonts.googleapis.com']);
             $this->addDirective(Directive::FONT, ['self','fonts.gstatic.com']);
             $this->addDirective(Directive::DEFAULT, ['self','google-analytics.com','unsafe-inline']);
