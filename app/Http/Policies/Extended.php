019ddb2a52ee35627928e8d57bfe9cd21c64fd9f
Sobandi - HCM <sobandi.afandi@capcx.com>
2024-06-19 08:20:37 +0700

    update

--- app/Http/Policies/Extended.php
+++ app/Http/Policies/Extended.php
@@ -10,24 +10,13 @@ class Extended extends Basic
     {
         parent::configure();
 
-        // $this
-        //     ->addDirective(Directive::STYLE, 'fonts.googleapis.com')
-        //     ->addDirective(Directive::DEFAULT, 'google.analytics.com')
-        //     ->addDirective(Directive::FONT, 'self')
-        //     ->addDirective(Directive::FONT, 'fonts.googleapis.com')
-        //     ->addDirective(Directive::FONT, 'fonts.gstatic.com')
-        //     ->addDirective(Directive::SCRIPT_ELEM, 'self')
-        //     ->addDirective(Directive::STYLE_ELEM, 'self');
-        //     ->addNonceForDirective(Directive::SCRIPT_ELEM)
-        //     ->addNonceForDirective(Directive::STYLE_ELEM);
-
             $this->addDirective(Directive::STYLE, 'fonts.googleapis.com');
-            $this->addDirective(Directive::STYLE_ELEM, ['self', 'fonts.googleapis.com', 'fonts.gstatic.com', 'google-analytics.com', 'ajax.googleapis.com', 'google.com', 'gstatic.com', 'connect.facebook.net', 'facebook.com', 'unsafe-inline']);
+            $this->addDirective(Directive::STYLE_ELEM, ['self', 'fonts.googleapis.com', 'unsafe-inline']);
             $this->addDirective(Directive::STYLE_ATTR, 'self', 'unsafe-inline', 'fonts.googleapis.com');
             $this->addDirective(Directive::FONT, 'fonts.googleapis.com');
             $this->addDirective(Directive::FONT, 'fonts.gstatic.com');
             $this->addDirective(Directive::FONT, 'self');
-            $this->addDirective(Directive::DEFAULT, ['self', 'fonts.googleapis.com', 'fonts.gstatic.com', 'google-analytics.com', 'ajax.googleapis.com', 'google.com', 'gstatic.com', 'connect.facebook.net', 'facebook.com', 'unsafe-inline']);
+            $this->addDirective(Directive::DEFAULT, ['self','unsafe-inline']);
 
             // $this->reportOnly();
     }
