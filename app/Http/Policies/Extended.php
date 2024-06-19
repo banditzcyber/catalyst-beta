f13cda8e0346db79ce4e9c74ecc4c5efebc7ab05
Sobandi - HCM <sobandi.afandi@capcx.com>
2024-06-19 10:43:28 +0700

    update

--- app/Http/Policies/Extended.php
+++ app/Http/Policies/Extended.php
@@ -22,6 +22,8 @@ class Extended extends Basic
                 ->addDirective(Directive::SCRIPT, 'self')
                 ->addDirective(Directive::STYLE, ['self','fonts.googleapis.com'])
                 ->addDirective(Directive::FONT, ['self','fonts.gstatic.com'])
+                ->addDirective(Directive::STYLE_ELEM, ['self','fonts.googleapis.com', 'unsafe-inline'])
+                ->addDirective(Directive::DEFAULT, ['self','fonts.googleapis.com', 'unsafe-inline'])
                 ->addNonceForDirective(Directive::SCRIPT)
                 ->addNonceForDirective(Directive::STYLE);
     }
