f9cb37b0531643d8b29ec906ea3fc4d688bedf94
Sobandi - HCM <sobandi.afandi@capcx.com>
2024-06-21 14:02:08 +0700

    update

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -40,7 +40,7 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \App\Http\Middleware\VisitorMiddleware::class,
-            \Spatie\Csp\AddCspHeaders::class,
+            // \Spatie\Csp\AddCspHeaders::class,
 
 
 
