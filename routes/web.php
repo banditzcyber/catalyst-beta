4744df08b513892fe2fc0b9973305d69aa45a20e
Sobandi - HCM <sobandi.afandi@capcx.com>
2024-06-24 14:43:13 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -77,7 +77,7 @@ Route::get('/connect', 'LoginController@handleAzureCallback');
 // Route::get('/',[LoginController::class,'signInForm'])->name('sign.in');
 Route::get('/', [LandingController::class, 'index'])->name('sign.in');
 // Route::get('/', [LoginController::class, 'microsoftOAuth'])->name('microsoft.oAuth');
-// Route::get('microsoft-oAuth', [LoginController::class, 'microsoftOAuth'])->name('microsoft.oAuth');
+Route::get('microsoft-oAuth', [LoginController::class, 'microsoftOAuth'])->name('microsoft.oAuth');
 Route::get('authenticate', [LoginController::class, 'microsoftOAuthCallback'])->name('microsoft.oAuth.callback');
 
 
