7f30af75fccee0f7bf4856c83f57c1e66011bcec
Sobandi - HCM <sobandi.afandi@capcx.com>
2024-06-24 16:06:19 +0700

    update

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -23,7 +23,7 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
 
         \App\Http\Middleware\HttpsProtocol::class,
-        // \Spatie\Csp\AddCspHeaders::class,
+        \Spatie\Csp\AddCspHeaders::class,
     ];
 
     /**
