00b6f80d75b03d53a1bca0d85241a93d24fb41fe
Sobandi - HCM <sobandi.afandi@capcx.com>
2024-06-25 11:17:18 +0700

    update

--- app/Http/Policies/Extended.php
+++ app/Http/Policies/Extended.php
@@ -10,17 +10,16 @@ class Extended extends Basic
     {
         parent::configure();
 
-            // $this->addDirective(Directive::STYLE, ['self','fonts.googleapis.com', 'fonts.gstatic.com', 'cdn.jsdelivr.net']);
-            // $this->addDirective(Directive::STYLE_ELEM, ['self','fonts.googleapis.com', 'fonts.gstatic.com','cdn.jsdelivr.net', 'unsafe-inline']);
-            // $this->addDirective(Directive::FONT, ['self','fonts.googleapis.com', 'fonts.gstatic.com','cdn.jsdelivr.net']);
-            // $this->addDirective(Directive::SCRIPT, ['self','fonts.googleapis.com','cdn.jsdelivr.net']);
-            // $this->addDirective(Directive::SCRIPT_ELEM, ['self','cdn.jsdelivr.net','unsafe-inline']);
-            // $this->addDirective(Directive::DEFAULT, 'unsafe-inline');
+            $this
+                ->addDirective(Directive::STYLE, ['self','fonts.googleapis.com', 'fonts.gstatic.com', 'cdn.jsdelivr.net'])
+                ->addDirective(Directive::STYLE_ELEM, ['self','fonts.googleapis.com', 'fonts.gstatic.com','cdn.jsdelivr.net', 'unsafe-inline'])
+                ->addDirective(Directive::FONT, ['self','fonts.googleapis.com', 'fonts.gstatic.com','cdn.jsdelivr.net'])
+                ->addDirective(Directive::SCRIPT, ['self','fonts.googleapis.com','cdn.jsdelivr.net'])
+                ->addDirective(Directive::SCRIPT_ELEM, ['self','cdn.jsdelivr.net','unsafe-inline'])
+                ->addDirective(Directive::DEFAULT, 'unsafe-inline');
 
             // $this->reportOnly();
 
-            $this
-                // ->addDirective(Directive::STYLE, 'cdn.jsdelivr.net', 'unsafe-inline')
-                ->addDirective(Directive::SCRIPT, 'cdn.jsdelivr.net', 'unsafe-inline');
+
     }
 }
