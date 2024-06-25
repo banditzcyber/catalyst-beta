e50598dd93cf4053479213876cb57eef64012a64
Sobandi - HCM <sobandi.afandi@capcx.com>
2024-06-25 15:05:20 +0700

    update

--- app/Http/Policies/Extended.php
+++ app/Http/Policies/Extended.php
@@ -11,9 +11,9 @@ class Extended extends Basic
         parent::configure();
 
             $this
-                ->addDirective(Directive::STYLE, ['self','fonts.googleapis.com', 'fonts.gstatic.com', 'cdn.jsdelivr.net'])
+                ->addDirective(Directive::STYLE, ['self','fonts.googleapis.com', 'cdn.jsdelivr.net'])
                 ->addDirective(Directive::STYLE_ELEM, ['self','fonts.googleapis.com', 'fonts.gstatic.com','cdn.jsdelivr.net', 'unsafe-inline'])
-                ->addDirective(Directive::FONT, ['self','fonts.googleapis.com', 'fonts.gstatic.com','cdn.jsdelivr.net'])
+                ->addDirective(Directive::FONT, ['self','fonts.gstatic.com','cdn.jsdelivr.net'])
                 ->addDirective(Directive::SCRIPT, ['self','fonts.googleapis.com','cdn.jsdelivr.net'])
                 ->addDirective(Directive::SCRIPT_ELEM, ['self','cdn.jsdelivr.net','unsafe-inline']);
                 // ->addDirective(Directive::DEFAULT, 'unsafe-inline');
